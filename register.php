e3d8e8224c90bdd2ef99b0239fd6ff942e8f7b5f
arokito <kitokololoaron@gmail.com>
2021-02-16 07:43:01 +0300

    removed validation for name and anuani2

--- register.php
+++ register.php
@@ -9,13 +9,13 @@ $password = $_POST['password'];
 $repassword = $_POST['repassword'];
 $mobile = $_POST['mobile'];
 $address1 = $_POST['address1'];
-$address2 = $_POST['address2'];
-$name = "/^[A-Z][a-zA-Z ]+$/";
+// $address2 = $_POST['address2'];
+// $name = "/^[A-Z][a-zA-Z ]+$/";
 $emailValidation = "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9]+(\.[a-z]{2,4})$/";
 $number = "/^[0-9]+$/";
 
 if(empty($f_name) || empty($l_name) || empty($email) || empty($password) || empty($repassword) ||
-	empty($mobile) || empty($address1) || empty($address2)){
+	empty($mobile) || empty($address1) ){
 		
 		echo "
 			<div class='alert alert-warning'>
@@ -24,24 +24,24 @@ if(empty($f_name) || empty($l_name) || empty($email) || empty($password) || empt
 		";
 		exit();
 	} else {
-		if(!preg_match($name,$f_name)){
-		echo "
-			<div class='alert alert-warning'>
-				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-				<b>$f_name siyo jina!</b>
-			</div>
-		";
-		exit();
-	}
-	if(!preg_match($name,$l_name)){
-		echo "
-			<div class='alert alert-warning'>
-				<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
-				<b>$l_name siyo jina!</b>
-			</div>
-		";
-		exit();
-	}
+	// 	if(!preg_match($name,$f_name)){
+	// 	echo "
+	// 		<div class='alert alert-warning'>
+	// 			<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+	// 			<b>$f_name siyo jina!</b>
+	// 		</div>
+	// 	";
+	// 	exit();
+	// }
+	// if(!preg_match($name,$l_name)){
+	// 	echo "
+	// 		<div class='alert alert-warning'>
+	// 			<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>
+	// 			<b>$l_name siyo jina!</b>
+	// 		</div>
+	// 	";
+	// 	exit();
+	// }
 	if(!preg_match($emailValidation,$email)){
 		echo "
 			<div class='alert alert-warning'>
@@ -111,9 +111,9 @@ if(empty($f_name) || empty($l_name) || empty($email) || empty($password) || empt
 		$password = md5($password);
 		$sql = "INSERT INTO `user_info` 
 		(`user_id`, `first_name`, `last_name`, `email`, 
-		`password`, `mobile`, `address1`, `address2`) 
+		`password`, `mobile`, `address1`) 
 		VALUES (NULL, '$f_name', '$l_name', '$email', 
-		'$password', '$mobile', '$address1', '$address2')";
+		'$password', '$mobile', '$address1')";
 		$run_query = mysqli_query($con,$sql);
 		if($run_query){
 			echo "
