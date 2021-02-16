39d6763a6ff4c0de997a6007a1df0d348b7552c6
mibonyezo <mibonyezo@gmail.com>
2021-02-16 13:11:39 +0300

    added product_quantity variable

--- admin_area/index.php
+++ admin_area/index.php
@@ -208,6 +208,7 @@ if(isset($_POST['upload_button'])){
    $product_cat = $_POST['product_cat'];
    $product_brand = $_POST['product_brand'];
    $product_price = $_POST['product_price'];
+   $product_quantity = $_POST['product_quantity'];
    $product_desc = $_POST['product_desc'];
    $product_keywords = $_POST['product_keywords'];
    
@@ -217,7 +218,7 @@ if(isset($_POST['upload_button'])){
    
    move_uploaded_file($product_image_tmp,"product_images/$product_image");
    
-   $insert_product = "insert into products (product_cat,product_brand,product_title,product_price,product_desc,product_image,product_keywords) values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
+   $insert_product = "insert into products (product_cat,product_brand,product_title,product_price,product_quantity,product_desc,product_image,product_keywords) values ('$product_cat','$product_brand','$product_title','$product_price','$product_desc','$product_image','$product_keywords')";
    
    $run_product = mysqli_query($con,$insert_product);
    
