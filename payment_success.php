60c27c965f85fd5f05da47eefd1479db4906444f
J2B <jboniphace88@gmail.com>
2021-02-18 04:37:18 +0300

    fix more errors on taarifa link

--- payment_success.php
+++ payment_success.php
@@ -4,11 +4,11 @@ session_start();
 if(!isset($_SESSION["uid"])){
 	header("location:index.php");
 }
-$trx_id = $_GET["tx"];
-$p_st = $_GET["st"];
-$amt = $_GET["amt"];
-$cc = $_GET["cc"];
- $cm = $_GET["cm"];
+// $trx_id = $_GET["tx"];
+// $p_st = $_GET["st"];
+// $amt = $_GET["amt"];
+// $cc = $_GET["cc"];
+//  $cm = $_GET["cm"];
 //  $c_amt = $_COOKIE["ta"];
 ?>
 
