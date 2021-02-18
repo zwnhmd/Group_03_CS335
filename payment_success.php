a7aa76db5a2a308ab0cbb1de36980d2100fb6887
J2B <jboniphace88@gmail.com>
2021-02-18 07:04:59 +0300

    fix vifaa link on muamala taarifa page so as on click to return to vifaa page

--- payment_success.php
+++ payment_success.php
@@ -36,7 +36,7 @@ if(!isset($_SESSION["uid"])){
 			</div>
 			<ul class="nav navbar-nav">
 				<li><a href="profile.php"><span class="glyphicon glyphicon-home"></span> Nyumbani</a></li>
-				<li><a href="#"><span class="glyphicon glyphicon-tags"></span> Vifaa</a></li>
+				<li><a href="profile.php"><span class="glyphicon glyphicon-tags"></span> Vifaa</a></li>
 			</ul>
 		</div>
 	</div>
